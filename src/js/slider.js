227527923a2f9962e04c573ee17314862ef8b1cc
vadimfront <vadimzila94@gmail.com>
2023-02-15 00:20:04 +0200

    fixed bug in a slider's section

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,22 +3,19 @@ const $ = require("jquery");
 $(function () {
   $("#carousel_left").slick({
     infinite: true,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
     mobileFirst: true,
     variableWidth: true,
-    centerPadding: "205px",
-    centerMode: true,
     prevArrow: $(".carousel_left_arrow .prev"),
     nextArrow: $(".carousel_left_arrow .next"),
   });
   $("#carousel_right").slick({
     infinite: true,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
     mobileFirst: true,
     variableWidth: true,
-    centerPadding: "205px",
     prevArrow: $(".carousel_right_arrow > .prev"),
     nextArrow: $(".carousel_right_arrow > .next"),
   });
