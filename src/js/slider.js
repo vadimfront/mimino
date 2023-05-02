e7d23bf8b8edd86d7bee0ce56647ff4d84743412
vadimfront <vadimzila94@gmail.com>
2023-05-02 21:07:36 +0300

    fixed sliders in mobile version

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,19 +2,19 @@ const $ = require("jquery");
 import Swiper, { Navigation } from "swiper";
 
 $(function () {
-  const swiperLeft = new Swiper(".left_side .swiper", {
-    // Optional parameters
-    modules: [Navigation],
-    slidesPerView: "auto",
-    slidesPerGroup: 1,
-    spaceBetween: 0,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-      enabled: true,
-    },
-  });
-  const swiperRight = new Swiper(".right_side .swiper", {
+  // const swiperLeft = new Swiper(".left_side .swiper", {
+  //   // Optional parameters
+  //   modules: [Navigation],
+  //   slidesPerView: "auto",
+  //   slidesPerGroup: 1,
+  //   spaceBetween: 0,
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //     enabled: true,
+  //   },
+  // });
+  const swiperRight = new Swiper(".swiper", {
     // Optional parameters
     modules: [Navigation],
     slidesPerView: "auto",
